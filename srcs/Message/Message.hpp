d31ea747f1908d3e168b2e9180aa6ded4b77e9d5
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-08-02 09:27:14 -0400

    norminette

--- srcs/Message/Message.hpp
+++ srcs/Message/Message.hpp
@@ -6,7 +6,7 @@
 /*   By: sfournie <sfournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 10:31:25 by sfournie          #+#    #+#             */
-/*   Updated: 2022/08/01 17:17:48 by sfournie         ###   ########.fr       */
+/*   Updated: 2022/08/02 09:19:45 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,41 +33,57 @@
 
 using std::string;
 
+/*============================================================================*/
 namespace irc {
 
 class Message {
 
-private:	
+private:
+
+	Message( void ) : _client_ptr( NULL ) {  };				// default constructor [PRIVATE]
+	
+	/*--------------------ATTRIBUTES---------------------*/
+	
+	Message		_empty_object;								// call the default constructor to create an instance on the stack
 	Client*		_client_ptr;
 	string		_message_in;
 	string		_message_out;
 	
-	Message( void ) : _client_ptr( NULL ) {  };
 public:
-	Message( Client* client_ptr );
-	Message( const Message& rhs );
-	Message&	operator=( const Message& rhs );
-	~Message( void );
+
+	Message( Client* client_ptr );							// main constructor
+	Message( const Message& rhs );							// copy constructor
+	Message& operator=( const Message& rhs );				// copy operator overload
+	~Message( void );										// destructor
 	
+
+	/*---------------OTHER-OPERATOR-OVERLOAD--------------*/
+
 	bool	operator==( const Message& rhs) const;
 	string	operator[]( int rhs);
 
-	/* getters */
+	/*-----------------------GETTERS----------------------*/
+
 	Client*			get_client_ptr( void ) const;
 	const string&	get_message_in( void ) const;
 	const string&	get_message_out( void ) const;
 
-	/* setters */
+	/*-----------------------SETTERS----------------------*/
+
 	void	set_client_ptr( Client* client_ptr );
 
-	/* utils */
+
+	/*---------------OTHER-MEMBER-FUNCTIONS---------------*/
+
 	void	append_in( const string& str );
 	void	append_out( const string& str );
 
 	friend std::ostream&	operator<<( std::ostream& o, const Message& obj );
 };
 
-std::ostream&	operator<<( std::ostream& o, const Message& obj );
+	/*----------------NON-MEMBER-FUNCTIONS----------------*/
+	
+	std::ostream&	operator<<( std::ostream& o, const Message& obj );
 
 } // namespace irc end scope
 
