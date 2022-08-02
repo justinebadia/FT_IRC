4e5ec471012ab4e564c09b3dc623bddda9bbe060
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-08-02 12:37:31 -0400

    typedef

--- includes/typedef.hpp
+++ includes/typedef.hpp
@@ -6,7 +6,7 @@
 /*   By: sfournie <sfournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/31 12:54:52 by sfournie          #+#    #+#             */
-/*   Updated: 2022/08/01 17:10:11 by sfournie         ###   ########.fr       */
+/*   Updated: 2022/08/02 12:37:13 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,13 @@ typedef map<int, t_reply_function_ptr>		t_reply_map;
 // typedef map<string, string>				t_message_info; // WARNING: might not use it
 
 typedef struct	pollfd					t_pollfd;
-typedef struct	sockaddr_in6			t_addr6;
+typedef struct	sockaddr_in				t_addr;
 
 
 typedef struct s_socket
 {
-       t_pollfd        pollfd; // fd, events, revents
-       t_addr6         addr; 
+       t_pollfd			pollfd; // int fd, short events & revents
+       t_addr			addr; 
 }      t_socket;
 
 
