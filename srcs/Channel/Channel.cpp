43b08a31aa1d82afae7ed019e64312a04cae4421
Tokutaro Shimoda <tshimoda@c1r7p5.42quebec.com>
2022-08-05 18:13:39 -0400

    begin Channel pair<Client*, e_permission>

--- srcs/Channel/Channel.cpp
+++ srcs/Channel/Channel.cpp
@@ -6,7 +6,7 @@
 /*   By: tshimoda <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 08:34:51 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/08/02 08:45:24 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/08/05 18:09:28 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 using namespace irc;
 
-Channel::Channel( void ){} // default constructor [PRIVATE]
+Channel::Channel( void ){}														// default constructor [PRIVATE]
 
-Channel::Channel( const Channel& other ){ *this = other; } // copy constructor [PRIVATE]
+Channel::Channel( const Channel& other ){ *this = other; }						// copy constructor [PRIVATE]
 
-Channel& Channel::operator=( const Channel& other ){} // copy operator overload [PRIVATE]
+Channel& Channel::operator=( const Channel& other ){}							// copy operator overload [PRIVATE]
 
-Channel::Channel( const string& channel_name, Client* channel_owner ) // public-channel (no password) constructor
-	: _channel_name(channel_name)
-	, _channel_owner(channel_owner)
+Channel::Channel( const string& channel_name, Client* channel_owner )			// public-channel (no password) constructor
+	: _name(channel_name)
+	, _owner(channel_owner)
+	, _invite_only(false)
 	, _password_required(false)
 	, _channel_password(NULL)
-	, _nb_of_operators(1)
-
 {
+	memberlist
 	Channel::_nb_of_channels++;
 }
 
 Channel::Channel( const string& channel_name, Client* channel_owner, const string& channel_password ) // private-channel (PASSWORD) constructor
-	: _channel_name(channel_name)
-	, _channel_owner(channel_owner)
+	: _name(channel_name)
+	, _owner(channel_owner)
+	, _invite_only(false)
 	, _password_required(true)
-	, _channel_password(channel_password)
-	, _nb_of_operators(1)
+	, _password(channel_password)
 {
 	Channel::_nb_of_channels++;
 }
@@ -48,15 +48,74 @@ Channel::~Channel( void ) // destructor
 
 
 /*-----------------------GETTERS----------------------*/
-const string&	Channel::get_channel_name( void ) const { return _channel_name; }
-Client*			Channel::get_channel_owner( void ) const { return _channel_owner; } 
-const bool		Channel::get_is_password_required( void ) const { return _password_required; }
-const string&	Channel::get_channel_password( void ) const { return _channel_password; }
+
+const string&	Channel::get_name( void ) const { return _name; }
+
+Client*			Channel::get_owner( void ) const { return _owner; } 
+
+bool			Channel::get_is_invite_only( void ) const { return _invite_only; }
+
+bool			Channel::get_is_password_required( void ) const { return _password_required; }
+
+const string&	Channel::get_channel_password( void ) const { return _password; }
+
+list<Client*, e_permission>	Channel::get_memberlist( void ) { return _memberlist; }
+
+e_permission	Channel::get_permission( Client* client )
+{
+	list<Client*, e_permission>::iterator it = _memberlist.begin();
+	list<Client*, e_permission>::iterator ite = _memberlist.end();
+
+	for (; it != ite; it++)
+	{
+		if ((*it).first == client)
+			return (*it).second;					// list< pair <Client*, e_permission> >
+	}
+	
+	// std<<cout << "Client* not found in the _memberlist" << std::endl;
+	return FAIL;
+}
+
+
+static int	Channel::get_nb_of_channels( void ) { return _nb_of_channels; }
+
+
 
 /*-----------------------SETTERS----------------------*/
 
+void	Channel::set_invite_only( bool setting ) 
+{
+	_invite_only = setting;
+}
+
+void	Channel::set_password( const string& password )
+{
+		_password_required = true;
+		_password = password;
+}
+
+int		Channel::set_permission( Client* client, e_permission type )
+{	
+	list<Client*, e_permission>::iterator it = _memberlist.begin();
+	list<Client*, e_permission>::iterator ite = _memberlist.end();
+
+	for (; it != ite; it++)
+	{
+		if ((*it).first == client)
+			(*it).second = type;
+		return SUCCESS;
+	}
+	return FAIL;
+}
 
 /*---------------OTHER-MEMBER-FUNCTIONS---------------*/
+
+
+bool	Channel::is_empty( void )
+{
+	if (_memberlist.size() == 0)
+		// call the destructor
+}
 
 
 /*----------------NON-MEMBER-FUNCTIONS----------------*/
--- srcs/Channel/Channel.hpp
+++ srcs/Channel/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: sfournie <sfournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 08:24:20 by tshimoda          #+#    #+#             */
-/*   Updated: 2022/08/02 14:39:59 by sfournie         ###   ########.fr       */
+/*   Updated: 2022/08/05 17:57:22 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,64 +15,82 @@
 
 #include <iostream>
 #include <list>
+#include <map>
 #include <string>
 
 class Client; 
 
 using std::string;
 using std::list;
+using std::pair;
 
 /*============================================================================*/ 
 namespace irc {
 
 class Channel {
 
+
+enum e_permission													// Warning: test
+{
+	OWNER = 1,
+	OPERATOR = 2,
+	REGULAR = 4,
+	BAN = 8
+}
+
 private:
 
-	Channel( void ); 							// default constructor
-	Channel( const Channel& other );			// copy constructor
-	Channel& operator=( const Channel& other ); // copy operator overload
+	Channel( void ); 												// default constructor
+	Channel( const Channel& rhs );									// copy constructor
+	Channel& operator=( const Channel& rhs );	 					// copy operator overload
 
 	/*--------------------ATTRIBUTES---------------------*/
 	
-	const string	_channel_name;
-	Client*			_channel_owner;
-	const bool		_password_required;
-	const string	_channel_password;
-	list<Client*>	_channel_clientlist;
-	list<string>	_banlist; 					// banlist of nicknames;
+	const string						_name;						// _name for channel name
+	Client*								_owner;
+	bool								_invite_only;
+	bool								_password_required;
+	const string						_password;
+	list<pair<Client*, e_permission>	_memberlist;				// Warning: test
 
-	static	int		_nb_of_channels;
-	int				_nb_of_operators;
+	static	int							_nb_of_channels;
 
 
 public:
 
-	Channel( const string& channel_name, Client* channel_owner ); 								 	// public-channel (no password) constructor
-	Channel( const string& channel_name, Client* channel_owner, const string& channel_password );	// private-channel (PASSWORD) constructor
-	~Channel( void );																				// destructor
+	Channel( const string& channel_name, Client* channel_owner ); 	// no password constructor
+	Channel( const string& channel_name, Client* channel_owner, const string& channel_password ); // password required constructor
+	~Channel( void );												// destructor
 
 
 	/*-----------------------GETTERS----------------------*/
 
-	const string&	get_channel_name( void ) const;
-	Client*			get_channel_owner( void ) const;
-	const bool		get_is_password_required( void ) const;
-	const string&	get_channel_password( void ) const;
-	list<Client*>	get_channel_clientlist( void );
-	list<string>	get_channel_banlist( void );
-	static int		get_nb_of_channels( void );
-	int				get_nb_of_operators( void ) const;
+	const string&						get_name( void ) const;
+	Client*								get_owner( void ) const;
+	bool								get_is_invite_only( void ) const;
+	bool								get_is_password_required( void ) const;
+	string&								get_password( void ) const;
+	list<pair<Client*, e_permission> >	get_memberlist( void );
+	e_permission						get_permission( Client* client );
+
+	static int							get_nb_of_channels( void );
 
 	/*-----------------------SETTERS----------------------*/
 
-
+	void	set_invite_only( bool setting );
+	void	set_password( const string& password );
+	int		set_permission( Client* client, e_permission type );
 
 
 	/*---------------OTHER-MEMBER-FUNCTIONS---------------*/
 
+	int		add_member( Client* client );
+	int		remove_member( Client* client );
+	int		promote_member( Client* client );
+	int		demote_member( Client* client );
+	int		ban_member( Client* client );
 
-
+	bool	is_empty( void );	// if the memberlist is empty, destroy the channel 
 
 };
 
