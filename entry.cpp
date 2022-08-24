34edb3a836073a7f295876d82a9055142a0cc81d
Sebastien Fournier <sfournie@c2r2p2.42quebec.com>
2022-08-24 16:50:08 -0400

    added port validation

--- entry.cpp
+++ entry.cpp
@@ -6,7 +6,7 @@
 /*   By: sfournie <sfournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 11:52:15 by sfournie          #+#    #+#             */
-/*   Updated: 2022/08/24 15:39:18 by sfournie         ###   ########.fr       */
+/*   Updated: 2022/08/24 16:46:09 by sfournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,38 @@ using std::string;
 using irc::t_pollfd;
 using irc::Server;
 
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+
+    while (it != s.end() && std::isdigit(*it))
+		++it;
+    return !s.empty() && it == s.end();
+}
+
 int	main( int argc, char** argv )
 {
+	int	port;
+
 	if (argc != 3)
 	{
-		cerr << RED << "\v [Error: Usage:] " << RESET << "./ircserv <port> <pass> \v" << endl;
+		cerr << RED << "\v [Error: Usage:] " << RESET << "./ircserv <port> <pass>" << endl << "Valid port range: [6665-6669]\v" << endl;
 		return FAIL;
 	}
 	try 
 	{
+		if (is_number(string(argv[1])) == false)
+		{
+			cerr << "[Error: port:] not a number\v" << endl;
+			return FAIL;
+		}
+		port = std::stoi(string(argv[1]));
+		if (port < 6665 || port > 6669)
+		{
+			cerr << "[Error: port:] Valid port range: [6665-6669]\v" << endl;
+			return FAIL;
+		}
+
 		Server& server = Server::get_server(static_cast<u_int>(std::stoi(string(argv[1]))), argv[2]);
 		server.set_signal_ctrl_c();
 		// check if server did get created
